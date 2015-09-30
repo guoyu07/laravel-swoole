bb696be6731141d07a31409cfa8e1c1b75d854a7
huyanping <hypxm@qq.com>
2015-09-30 10:01:44 +0800

    add Websocket

---
+++ src/WebSocket/Start.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/9/30
+ * Time: 9:21
+ */
+namespace LaravelSwoole\WebSocket;
+
+use Illuminate\Console\Command;
+
+class Start extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'swoole:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'start websocket server';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
